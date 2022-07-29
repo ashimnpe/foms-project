34d4196049231de153402bb878b227c181cf9da7
dipenchand <personaldipen@gmail.com>
2022-07-29 15:12:29 +0545

    Restore migrations

---
+++ oms-api/database/migrations/2022_07_28_170024_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->double('net_total');
+            $table->double('discount')->default(0);
+            $table->double('grand_total');
+            $table->string('order_status')->default('processing');
+            $table->integer('payment_method')->nullable();
+            $table->string('payment_status')->default('NotPaid');
+            $table->tinyInteger('cashier')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
